d86905d1bbd4ec079a4c044ac6a565cf73ea6daa
sinhua0201 <gohsinhua@gmail.com>
2025-05-21 22:13:08 +0800

    123

--- db.js
+++ db.js
@@ -18,13 +18,9 @@ const poolPromise = new sql.ConnectionPool(config)
     console.log('✅ Connected to Azure SQL Database');
     return pool;
   })
-  .catch(err => {
-    console.error('❌ Database connection failed:', err);
-    throw err; // 💥 抛出错误阻止使用 undefined 的 pool
-  });
+  .catch(err => console.error('❌ Database connection failed:', err));
 
 module.exports = {
   sql,
   poolPromise,
 };
-
